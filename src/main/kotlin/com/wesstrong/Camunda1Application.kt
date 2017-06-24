fc0f94bfd32450b51f28a88b01f508dc4a147dc4
Stephen Newport <SBN@economical.com>
2017-06-24 12:09:34 -0400

    Initial commit

---
+++ src/main/kotlin/com/wesstrong/Camunda1Application.kt
@@ -0,0 +1,16 @@
+package com.wesstrong
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+
+
+@SpringBootApplication
+class Camunda1Application
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Camunda1Application::class.java, *args)
+
+}
---
+++ src/test/kotlin/com/wesstrong/Camunda1ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.wesstrong
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Camunda1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
